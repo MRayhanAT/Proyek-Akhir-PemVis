458a37f914d611ae89ab57e26487eed646a50ddf
EXOGAMER007 <daffa8110@gmail.com>
2025-05-12 18:38:53 +0800

    update

--- PA PEMVIS/obj/Debug/net8.0-windows/PA PEMVIS.AssemblyInfo.vb
+++ PA PEMVIS/obj/Debug/net8.0-windows/PA PEMVIS.AssemblyInfo.vb
@@ -16,7 +16,7 @@ Imports System.Reflection
 <Assembly: System.Reflection.AssemblyCompanyAttribute("PA PEMVIS"),  _
  Assembly: System.Reflection.AssemblyConfigurationAttribute("Debug"),  _
  Assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0"),  _
- Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0"),  _
+ Assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13ee81c090c1915ffd5d6b9707676708bedcb586"),  _
  Assembly: System.Reflection.AssemblyProductAttribute("PA PEMVIS"),  _
  Assembly: System.Reflection.AssemblyTitleAttribute("PA PEMVIS"),  _
  Assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0"),  _
