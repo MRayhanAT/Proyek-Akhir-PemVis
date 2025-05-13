92c1bd42ecab3bcb84d93d1f6021727fdd2e9099
EXOGAMER007 <daffa8110@gmail.com>
2025-05-13 16:10:03 +0800

    database

---
+++ DATABASE/DBProyekAkhirPemVis.sql
@@ -0,0 +1,24 @@
+CREATE SCHEMA ProyekAkhirPemVis;
+
+USE ProyekAkhirPemVis;
+
+Create table tbUsers (
+	NomorHP bigint primary key,
+    username  varchar(50),
+    password varchar(50),
+	KTP  varchar(300),
+    statusPinjaman varchar(50) default "tidak ada pinjaman"
+);
+
+Create table Pinjaman (
+	idPinjaman int primary key,
+    Nama varchar(50),
+    Alamat varchar(100),
+    Nominal int,
+    cicilan varchar(50) default "pending",
+    NomorHP_Pengguna bigint,
+    FOREIGN KEY (NomorHP_Pengguna) REFERENCES tbUsers(NomorHP)
+);
+
+INSERT INTO tbUsers (NomorHP, username, password, KTP, statusPinjaman)
+VALUES (6281234567890, 'budi123', 'rahasia123', '3578012345678901', 'tidak ada pinjaman');
