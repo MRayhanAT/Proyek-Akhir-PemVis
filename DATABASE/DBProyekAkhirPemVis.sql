92ba625723f5237ad4e4f88e48767d3cb4e65579
EXOGAMER007 <daffa8110@gmail.com>
2025-05-14 13:21:16 +0800

    update database

--- DATABASE/DBProyekAkhirPemVis.sql
+++ DATABASE/DBProyekAkhirPemVis.sql
@@ -15,10 +15,13 @@ Create table Pinjaman (
     Nama varchar(50),
     Alamat varchar(100),
     Nominal int,
-    cicilan varchar(50) default "pending",
+    cicilan varchar(50),
+    status varchar(50) default "pending", 
     NomorHP_Pengguna bigint,
     FOREIGN KEY (NomorHP_Pengguna) REFERENCES tbUsers(NomorHP)
 );
+
+select * from tbUsers;
 
 INSERT INTO tbUsers (NomorHP, username, password, KTP, statusPinjaman)
 VALUES (6281234567890, 'budi123', 'rahasia123', '3578012345678901', 'tidak ada pinjaman');
