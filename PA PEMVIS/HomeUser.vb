59c152ca1c2902644a38c37792d32258d31dc397
Ayyaayyawaeee <162291442+Ayyaayyawaeee@users.noreply.github.com>
2025-05-15 08:56:39 +0800

    Update HomeUser.vb

--- PA PEMVIS/HomeUser.vb
+++ PA PEMVIS/HomeUser.vb
@@ -1,3 +1,42 @@
-﻿Public Class HomeUser
+Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 
+Public Class HomeUser
+    Private Sub BtnTambah_Click(sender As Object, e As EventArgs) Handles BtnTambah.Click
+        Dim tugas As String = txtList.Text.Trim()
+        If tugas <> "" Then
+            Todolistbox.Items.Add(tugas)
+            txtList.Clear()
+        Else
+            MessageBox.Show("Masukkan tugas terlebih dahulu.")
+        End If
+    End Sub
+
+    Private Sub Todolistbox_DrawItem(sender As Object, e As DrawItemEventArgs) Handles Todolistbox.DrawItem
+        If e.Index < 0 Then Exit Sub
+
+        Dim itemText As String = Todolistbox.Items(e.Index).ToString()
+
+        e.DrawBackground()
+        e.Graphics.FillRectangle(New SolidBrush(Color.LightYellow), e.Bounds)
+
+        Dim checked As Boolean = Todolistbox.GetItemChecked(e.Index)
+
+        CheckBoxRenderer.DrawCheckBox(e.Graphics, New Point(e.Bounds.X + 2, e.Bounds.Y + 1),
+        If(checked, VisualStyles.CheckBoxState.CheckedNormal, VisualStyles.CheckBoxState.UncheckedNormal))
+
+        Dim textBrush As Brush = Brushes.Black
+        e.Graphics.DrawString(itemText, e.Font, textBrush, e.Bounds.X + 20, e.Bounds.Y)
+
+        e.DrawFocusRectangle()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FMenu.Show()
+        Me.Hide()
+    End Sub
+
+    Private Sub BtnAjukan_Click(sender As Object, e As EventArgs) Handles BtnAjukan.Click
+        AjukanPijaman.Show()
+        Me.Hide()
+    End Sub
 End Class
